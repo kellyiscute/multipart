49e2fa044aa19a6b49968c2c9e8be583c162ed21
Kelly <guojiyou40020@hotmail.com>
2021-05-28 17:42:50 +0800

    init commit

---
+++ lib/src/cached_bytes_builder.dart
@@ -0,0 +1,144 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'dart:typed_data';
+
+import 'package:nanoid/async.dart';
+import 'package:path/path.dart' as path;
+
+class CachedBytesBuilder {
+  int cachingSize = 10485760; // 10MB
+  File? _cacheFile;
+  int _length = 0;
+  final List<Uint8List> _chunks = [];
+
+  bool get isCached => _cacheFile != null;
+  Future<String> get text async => Utf8Decoder().convert(await toBytes());
+  Future<dynamic> get json async => jsonDecode(await text);
+
+  CachedBytesBuilder({int? cacheStart}) {
+    if (cacheStart != null) {
+      cachingSize = cacheStart;
+    }
+  }
+
+  Future<void> add(List<int> bytes) async {
+    Uint8List typedBytes;
+    if (bytes is Uint8List) {
+      typedBytes = bytes;
+    } else {
+      typedBytes = Uint8List.fromList(bytes);
+    }
+    _length += typedBytes.length;
+
+    if (_cacheFile != null) {
+      this._cacheFile = await _cacheFile!
+          .writeAsBytes(bytes, mode: FileMode.append, flush: true);
+      return;
+    }
+
+    if (_length > cachingSize) {
+      _cacheFile = File.fromUri(Uri.parse(
+          path.join(Directory.systemTemp.path, await nanoid(16) + ".cache")));
+
+      _cacheFile = await _cacheFile!.writeAsBytes(await this.takeBytes());
+      _cacheFile = await _cacheFile!.writeAsBytes(typedBytes);
+      return;
+    }
+    _chunks.add(typedBytes);
+  }
+
+  Future<void> addByte(int byte) async {
+    _length++;
+    if (_cacheFile != null) {
+      _cacheFile = await _cacheFile!.writeAsBytes([byte]);
+      return;
+    }
+    _chunks.add(Uint8List(1)..[0] = byte);
+  }
+
+  Future<Uint8List> takeBytes() async {
+    if (_cacheFile != null) {
+      var data = await _cacheFile!.readAsBytes();
+      await _cacheFile!.delete();
+      return data;
+    }
+
+    if (_length == 0) return Uint8List(0);
+    if (_chunks.length == 1) {
+      var buffer = _chunks[0];
+      _clear();
+      return buffer;
+    }
+    var buffer = Uint8List(_length);
+    int offset = 0;
+    for (var chunk in _chunks) {
+      buffer.setRange(offset, offset + chunk.length, chunk);
+      offset += chunk.length;
+    }
+    _clear();
+    return buffer;
+  }
+
+  Future<Uint8List> toBytes() async {
+    if (_cacheFile != null) {
+      return _cacheFile!.readAsBytes();
+    }
+
+    if (_length == 0) return Uint8List(0);
+    var buffer = Uint8List(_length);
+    int offset = 0;
+    for (var chunk in _chunks) {
+      buffer.setRange(offset, offset + chunk.length, chunk);
+      offset += chunk.length;
+    }
+    return buffer;
+  }
+
+  Future<void> save(Uri path) async {
+    if (_cacheFile != null) {
+      await _cacheFile!.rename(_cacheFile!.uri.resolveUri(path).path);
+    }
+    await File.fromUri(path).writeAsBytes(await this.takeBytes());
+  }
+
+  Future<Uint8List> readBytes(int length) async {
+    BytesBuilder builder = BytesBuilder();
+    if (isCached) {
+      await for (var data in this._cacheFile!.openRead().take(length)) {
+        builder.add(data);
+      }
+      return builder.toBytes();
+    } else {
+      int pos = 0;
+      while (builder.length < length) {
+        int remaining = length - builder.length;
+        if (_chunks[pos].lengthInBytes > remaining) {
+          builder.add(_chunks[pos].getRange(0, remaining).toList());
+        } else {
+          builder.add(_chunks[pos]);
+        }
+      }
+    }
+    return builder.toBytes();
+  }
+
+  int get length => _length;
+
+  bool get isEmpty => _length == 0;
+
+  bool get isNotEmpty => _length != 0;
+
+  Future<void> clear() async {
+    await _clear();
+  }
+
+  Future<void> _clear() async {
+    if (_cacheFile != null) {
+      await _cacheFile!.delete();
+      return;
+    }
+    _length = 0;
+    _chunks.clear();
+  }
+}
---
+++ lib/src/multipart.dart
@@ -0,0 +1,159 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:bytes_io/bytes_io.dart';
+import 'package:multipart/src/cached_bytes_builder.dart';
+
+/// input --------------
+///           pipe      \
+///                    cache
+class _BoundaryFinderPipe {
+  List<int> pipe = [];
+  Uint8List cached = Uint8List(1048576); // 1MB as a chunk
+  int position = 0;
+  final String boundary;
+  final int newLineChar = AsciiEncoder().convert("\n").first;
+  final int boundaryLastChar;
+  final Utf8Decoder decoder = Utf8Decoder();
+  final CachedBytesBuilder cachedBytesBuilder = CachedBytesBuilder();
+  final Future<void> Function(Uint8List, CachedBytesBuilder)
+      boundaryReachedCallback;
+  final BytesBuilder headerBytes = BytesBuilder();
+  bool findHeaders = false;
+
+  _BoundaryFinderPipe(String boundary, this.boundaryReachedCallback)
+      : this.boundary = "\n" + boundary,
+        boundaryLastChar = boundary.codeUnitAt(boundary.length - 1);
+
+  Future<void> put(int value) async {
+    if (findHeaders) {
+      if (pipe.length == 3 && pipe.where((e) => e == newLineChar).length == 3) {
+        headerBytes.add(pipe);
+        pipe.clear();
+        findHeaders = false;
+      } else {
+        pipe.removeAt(0);
+        pipe.add(value);
+      }
+      return;
+    }
+
+    if (pipe.length > this.boundary.length) {
+      cached[position] = pipe[0];
+      position++;
+      pipe.removeAt(0);
+    }
+    pipe.add(value);
+    // cache size       ⬇️
+    if (position == 1048575) {
+      await cachedBytesBuilder.add(cached);
+      cached.clear();
+    }
+    // statistically if the first and the last char matches
+    // it likely is the boundary
+    // no data supported!
+    if (pipe.first == newLineChar && pipe.last == boundaryLastChar) {
+      if (decoder.convert(pipe) == boundary) {
+        // push all cached bytes
+        await cachedBytesBuilder.add(cached.getRange(0, position).toList());
+        cached.clear();
+        headerBytes.clear();
+        await this
+            .boundaryReachedCallback(headerBytes.toBytes(), cachedBytesBuilder);
+        findHeaders = true;
+      }
+    }
+  }
+}
+
+class MultipartContent {
+  final String field;
+  final String? filename;
+  final CachedBytesBuilder content;
+  final String? contentType;
+
+  MultipartContent({
+    required this.field,
+    this.filename,
+    required this.content,
+    this.contentType,
+  });
+}
+
+class Multipart {
+  final HttpRequest request;
+  late final String boundary;
+  int _boundaryFound = 0;
+  List<MultipartContent> content = [];
+
+  Multipart(this.request) {
+    if (request.headers.contentType == null) {
+      throw Exception("content-type not present in headers");
+    }
+    if (request.headers.contentType!.mimeType != "multipart/form-data") {
+      throw Exception("Damn it! Not a multipart");
+    }
+    assert(
+      request.headers.contentType!.parameters.containsKey("boundary"),
+      "malformed multipart",
+    );
+    boundary = request.headers.contentType!.parameters["boundary"]!;
+  }
+
+  Future<void> call() async {
+    await load();
+  }
+
+  Future<void> _onBoundaryReached(
+      Uint8List headerBytes, CachedBytesBuilder data) async {
+    if (_boundaryFound == 0) {
+      _boundaryFound++;
+      return;
+    }
+
+    var headers = getFormDataHeader(headerBytes);
+    MultipartContent content = MultipartContent(
+      field: headers["name"]!,
+      content: data,
+      filename: headers["filename"],
+      contentType: headers["content-type"],
+    );
+    this.content.add(content);
+  }
+
+  Future<List<MultipartContent>> load() async {
+    final _BoundaryFinderPipe pipe =
+        _BoundaryFinderPipe(boundary, _onBoundaryReached);
+
+    await for (var data in request) {
+      BytesReader reader = BytesReader.fromUint8List(data);
+      while (reader.position < reader.data.length) {
+        pipe.put(reader.readByte());
+      }
+    }
+    return content;
+  }
+
+  Map<String, String> getFormDataHeader(Uint8List data) {
+    BytesReader reader = BytesReader.fromUint8List(data);
+    String line;
+    // group(1) => key, group(4) => value
+    RegExp pattern = RegExp(r"([A-z-]*)((:\s)|=)([^;$]*)(;|$)");
+    Utf8Decoder decoder = Utf8Decoder();
+    Map<String, String> params = {};
+    do {
+      line = decoder.convert(reader.readUntil("\n".codeUnitAt(0)));
+      params.addAll(
+        Map.fromEntries(
+          pattern.allMatches(line).map<MapEntry<String, String>>(
+                (e) => MapEntry(e.group(1).toString(), e.group(4).toString()),
+              ),
+        ),
+      );
+      // dispose the '\n'
+      reader.readByte();
+    } while (line != "");
+    return params;
+  }
+}
